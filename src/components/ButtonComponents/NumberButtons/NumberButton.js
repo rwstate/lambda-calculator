9adaeee130697c086162e94609f12a88602e6e05
Ryan State <rwstate@gmail.com>
2019-10-08 15:18:51 -0400

    finish mvp

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  if (props.numbers === "0") {
+    return (    
+    <button className="number-button zero-button">
+    {props.numbers}
+  </button>)
+  }
   return (
-    <>
+    <button className="number-button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.numbers}
+    </button>
   );
 };
+
+export default NumberButton;
